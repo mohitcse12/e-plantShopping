52abcbb81b5e28c7cecf07ea2a0df6bb26c56a1e
mohitcse12 <mohitgupta12qw@gmail.com>
2026-06-29 20:24:04 +0530

    Add AboutUs component for nursery information

---
+++ src/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>Paradise Nursery is your one-stop destination for all kinds of beautiful plants. We believe in making the world greener and more peaceful, one plant at a time.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
